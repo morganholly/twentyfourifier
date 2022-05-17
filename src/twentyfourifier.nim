84254ce4d821e48b4dd07abe27351215cea46d64
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2022-05-17 02:25:04 -0400

    add an upscale method
    it works by averaging pixels for each new pixel, and then selecting the closest color. i don't really like it, or at least it doesn't fit the tile.
    
    . a b .
    c d e f
    g h i j
    . k l .
    
    d 1 e
    2 5 4
    h 3 i
    
    1 = avg(a,c,d,e)
    2 = avg(c,d,g,h)
    3 = avg(h,i,k,l)
    4 = avg(e,f,i,j)
    5 = avg(d,e,h,i)

--- src/twentyfourifier.nim
+++ src/twentyfourifier.nim
@@ -11,8 +11,58 @@ type
         b: uint8
         a: uint8
 
+proc `==`(left: RGBA, right: RGBA): bool =
+    result = (left.r == right.r) and (left.g == right.g) and (left.b == right.b) and (left.a == right.a)
+
+proc avg(left: RGBA, right: RGBA): RGBA =
+    result = RGBA(r: cast[uint8]((cast[int32](left.r) + cast[int32](right.r)) shr 1), g: cast[uint8]((cast[int32](left.g) + cast[int32](right.g)) shr 1), b: cast[uint8]((cast[int32](left.b) + cast[int32](right.b)) shr 1), a: cast[uint8]((cast[int32](left.a) + cast[int32](right.a)) shr 1))
+
+proc closest(opt: openArray[RGBA], other: RGBA): RGBA =
+    # c approximation
+    # var dists: seq[int32] = @[]
+    var min = int32.high
+    var minindex = -1
+    for i, c in enumerate(opt):
+        var rmean = ( cast[int32](c.r) + cast[int32](other.r) ) shr 1
+        var r = cast[int32](c.r) - cast[int32](other.r)
+        var g = cast[int32](c.g) - cast[int32](other.g)
+        var b = cast[int32](c.b) - cast[int32](other.b)
+        var dist = (((512+rmean)*r*r) shr 8) + 4*g*g + (((767-rmean)*b*b) shr 8)
+        if dist == 0:
+            return c
+        elif dist < min:
+            min = dist
+            minindex = i
+        # dists &= @[dist]
+    return opt[minindex]
+
+proc sum(val: RGBA): int32 =
+    result = (cast[int32](val.r) shl 24) + (cast[int32](val.g) shl 16) + (cast[int32](val.b) shl 8) + cast[int32](val.a)
+
+proc pick(ar: array[4, RGBA]): RGBA =
+    let c01 = sum(ar[0]) == sum(ar[1])
+    let c23 = sum(ar[2]) == sum(ar[3])
+    let c02 = sum(ar[0]) == sum(ar[2])
+    if c01 and c23 and c02:
+        result = ar[0]
+    elif c01 and c23:
+        result = closest(ar, avg(ar[0], ar[2]))
+    else:
+        let c13 = sum(ar[1]) == sum(ar[3])
+        if c02 and c13:
+            result = closest(ar, avg(ar[0], ar[1]))
+        let c12 = sum(ar[1]) == sum(ar[2])
+        let c30 = sum(ar[3]) == sum(ar[0])
+        if c12 and c30:
+            result = closest(ar, avg(ar[1], ar[3]))
+        else:
+            result = closest(ar, avg(avg(ar[0], ar[1]), avg(ar[2], ar[3])))
+
 proc read2d(png: PNGResult[string], x, y, w: int): char =
     result = png.data[y * w + x]
+
+proc scale(x: int): int =
+    result = (x shr 1) * 3
 
 let png = loadPNG(testfile, LCT_RGBA, 8)
 
@@ -30,6 +80,9 @@ echo(len(png.data))
 assert len(png.data) mod 4 == 0
 assert png.width mod 2 == 0
 assert png.height mod 2 == 0
+let outwidth = scale(png.width)
+let outheight= scale(png.height)
+echo(outwidth, " ",  outheight)
 var image = newTensor[RGBA]([png.width, png.height])
 for y in 0 ..< png.height:
     for x in 0 ..< png.width:
@@ -39,11 +92,52 @@ for y in 0 ..< png.height:
                         b: cast[uint8](png.data[4 * (y * png.width + x) + 2]),
                         a: cast[uint8](png.data[4 * (y * png.width + x) + 3]))
 
+var upscaled = newTensor[RGBA]([outwidth, outheight])
+for y in countup(0, png.height-1, 2):
+    var ym1, yp2: int
+    if y == 0:
+        ym1 = y
+        yp2 = y + 1
+    elif y == png.height-1:
+        ym1 = y-1
+        yp2 = y
+    else:
+        ym1 = y-1
+        yp2 = y + 1
+    for x in countup(0, png.width-1, 2):
+        var xm1, xp2: int
+        if x == 0:
+            xm1 = x
+            xp2 = x + 1
+        elif x == png.width-1:
+            xm1 = x-1
+            xp2 = x
+        else:
+            xm1 = x-1
+            xp2 = x + 1
+        # echo("x: ", x, " y:", y)
+        # y,  x  y,  x+1
+        # y+1,x  y+1,x+1
+        # y,  x  y,  x+1  y,  x+2
+        # y+1,x  y+1,x+1  y+1,x+2
+        # y+2,x  y+2,x+1  y+2,x+2
+        var x3 = scale(x)
+        var y3 = scale(y)
+        upscaled[y3, x3] = image[y, x]
+        upscaled[y3, x3+1] = pick([image[ym1, x], image[ym1, x+1], image[y, x], image[y, x+1]])
+        upscaled[y3, x3+2] = image[y, x+1]
+        upscaled[y3+1, x3] = pick([image[y, xm1], image[y+1, xm1], image[y, x], image[y+1, x]])
+        upscaled[y3+1, x3+1] = pick([image[y+1, x], image[y+1, x+1], image[y, x], image[y, x+1]])
+        upscaled[y3+1, x3+2] = pick([image[y, xp2], image[y+1, xp2], image[y, x+1], image[y+1, x+1]])
+        upscaled[y3+2, x3] = image[y+1, x]
+        upscaled[y3+2, x3+1] = pick([image[yp2, x], image[yp2, x+1], image[y, x], image[y, x+1]])
+        upscaled[y3+2, x3+2] = image[y+1, x+1]
+
 var output: seq[uint8] = @[]
-for y in 0 ..< png.height:
-    for x in 0 ..< png.width:
-        output &= @[image[y, x].r, image[y, x].g, image[y, x].b, image[y, x].a]
-discard savePNG32(outputfile, output, png.width, png.height)
+for y in 0 ..< outheight:
+    for x in 0 ..< outwidth:
+        output &= @[upscaled[y, x].r, upscaled[y, x].g, upscaled[y, x].b, upscaled[y, x].a]
+discard savePNG32(outputfile, output, outwidth, outheight)
 
 #3d2a42ff #574658ff #3d2a42ff #3d2a42ff #3d2a42ff #574658ff #574658ff #b29767ff #cdc397ff #b29767ff #8e574bff #222435ff #3d2a42ff #3d2a42ff #643633ff #19131dff 
 #6e6979ff #6e6979ff #574658ff #3d2a42ff #959aa6ff #574658ff #3d2a42ff #8e574bff #b29767ff #643633ff #643633ff #8e574bff #3d2a42ff #3d2a42ff #3d2a42ff #222435ff 
